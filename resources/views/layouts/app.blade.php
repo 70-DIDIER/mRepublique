8cc6e672b38459c292fd35b8a8ff0ec568fc3cad
guinwogbegno koffi <koffi.guinwogbegno@ipnetinstitute.com>
2025-06-28 18:30:54 +0000

    ajout d'un favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>M'Republique</title>
-
+    
+    <link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}">
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('font-awesome/css/font-awesome.css') }}" rel="stylesheet">
     <link href="{{ asset('css/animate.css') }}" rel="stylesheet">
