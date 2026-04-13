258785063c140674d1172765bd34d0919e75a893
guinwogbegno koffi <koffi.guinwogbegno@ipnetinstitute.com>
2026-04-13 12:45:03 +0000

    gestion du role admin

--- app/Console/Commands/CreateAdminUser.php
+++ app/Console/Commands/CreateAdminUser.php
@@ -4,7 +4,6 @@ namespace App\Console\Commands;
 
 use App\Models\User;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class CreateAdminUser extends Command
@@ -32,7 +31,7 @@ class CreateAdminUser extends Command
         $user = User::create([
             'name'     => $name,
             'email'    => $email,
-            'password' => Hash::make($password),
+            'password' => $password,
             'role'     => 'admin',
         ]);
 
